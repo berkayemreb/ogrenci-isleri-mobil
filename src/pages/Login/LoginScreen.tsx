4835621e2e1953413562f5e6fcdfcfdcce439f05
bbogum19 <berkaye2001@hotmail.com>
2023-04-05 13:32:59 +0300

    Logo and font have been included in the project.

--- src/pages/Login/LoginScreen.tsx
+++ src/pages/Login/LoginScreen.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { SafeAreaView, View, Image, Text, TouchableOpacity } from 'react-native';
 import styles from './LoginScreenStyles';
-import { LoginScreenProps } from '../../navigation/types';
 
-const LoginScreen = ({ navigation }: LoginScreenProps) => {
-
-    const goToHomePage = () => {
-        navigation.navigate('Home');
-    }
+const LoginScreen = () => {
 
     return (
-        <View style={styles.container}>
-            <Text>LoginScreen Page!</Text>
-            <Button title='go to home' onPress={goToHomePage} />
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View>
+                <Image style={styles.image} source={require('../../../assets/images/logo-bg-white.png')} />
+            </View>
+            <View style={styles.buttons_container}>
+                <TouchableOpacity style={styles.login_container}>
+                    <Text style={styles.login_text}>Giriş Yap</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.signUp_container}>
+                    <Text style={styles.signUp_text}>Kayıt Ol</Text>
+                </TouchableOpacity>
+
+            </View>
+        </SafeAreaView>
     )
 }
 
--- src/pages/Login/LoginScreenStyles.tsx
+++ src/pages/Login/LoginScreenStyles.tsx
@@ -1,7 +1,54 @@
-import { StyleSheet } from "react-native";
+import { useFonts } from 'expo-font';
+import { Dimensions, StyleSheet } from "react-native";
+
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
+
+() => {
+    const [fontsLoaded] = useFonts({
+        'SommetRoundedRegular': require('../../../assets/fonts/SommetRoundedRegular.otf'),
+    });
+}
 
 export default StyleSheet.create({
     container: {
-        backgroundColor: 'red',
-    }
+        height: '50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+    image: {
+        width: width / 1.5,
+        height: height / 7,
+        resizeMode: 'contain',
+    },
+    buttons_container: {
+        marginTop: 5,
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    login_container: {
+        width: '30%',
+        alignItems: 'center',
+        backgroundColor: '#fcbe16',
+        padding: 7,
+    },
+    login_text: {
+        color: '#333333',
+        fontSize: 15,
+        fontFamily: 'SommetRoundedRegular',
+    },
+    signUp_container: {
+        marginLeft: 20,
+        width: '30%',
+        alignItems: 'center',
+        padding: 7,
+        backgroundColor: '#fcbe16',
+    },
+    signUp_text: {
+        color: '#333333',
+        fontSize: 15,
+        fontFamily: 'SommetRoundedRegular',
+    },
 });
+
