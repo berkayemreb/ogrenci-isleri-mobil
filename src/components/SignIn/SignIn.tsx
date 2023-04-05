4099ed6ae667f8d32449db9d6bfb91b08d0b9408
bbogum19 <berkaye2001@hotmail.com>
2023-04-05 19:27:04 +0300

    Firebase auth model is integrated into project.

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import { View, TextInput, TouchableOpacity, Text } from 'react-native';
 import styles from './SignInStyles';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase';
+
 
 const SignIn = () => {
 
-    const [eposta, setEposta] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
 
-    const onChangeEposta = (text: string) => {
-        setEposta(text);
+    const onChangeEmail = (text: string) => {
+        setEmail(text);
     }
 
     const onChangePassword = (text: string) => {
@@ -16,15 +19,28 @@ const SignIn = () => {
     }
 
     const onSubmit = () => {
-        console.log("Giriş yapıldı");
-
-        console.log({ eposta, password });
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log("Giriş başarıyla tamamlandı");
+                setEmail('');
+                setPassword('');
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log("Giriş yapılamadı");
+                setEmail('');
+                setPassword('');
+            });
     }
 
     return (
         <View style={styles.container}>
             <View>
-                <TextInput style={styles.input} cursorColor='#a0a0a0' selectionColor='#b5b5b5' placeholder='E-posta' onChangeText={onChangeEposta} value={eposta} autoCapitalize='none' />
+                <TextInput style={styles.input} cursorColor='#a0a0a0' selectionColor='#b5b5b5' placeholder='E-posta' onChangeText={onChangeEmail} value={email} autoCapitalize='none' />
                 <TextInput style={styles.input} cursorColor='#a0a0a0' selectionColor='#b5b5b5' placeholder='Şifre' secureTextEntry onChangeText={onChangePassword} value={password} autoCapitalize='none' />
             </View>
             <TouchableOpacity style={styles.button} onPress={onSubmit} >
--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import { View, TextInput, TouchableOpacity, Text } from 'react-native';
 import styles from './SignUpStyles';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase';
 
 const SignUp = () => {
 
-    const [eposta, setEposta] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
 
-    const onChangeEposta = (text: string) => {
-        setEposta(text);
+    const onChangeEmail = (text: string) => {
+        setEmail(text);
     }
 
     const onChangePassword = (text: string) => {
@@ -17,18 +19,31 @@ const SignUp = () => {
 
     const onSubmit = () => {
 
-        console.log("Üye olundu");
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log("üye olundu", user);
+                setEmail('');
+                setPassword('');
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+                console.log("Üye olunamadı");
+                setEmail('');
+                setPassword('');
 
-        console.log({ eposta, password });
+            });
 
-        setEposta('');
-        setPassword('');
     }
 
     return (
         <View style={styles.container}>
             <View>
-                <TextInput style={styles.input} cursorColor='#a0a0a0' selectionColor='#b5b5b5' placeholder='E-posta' onChangeText={onChangeEposta} value={eposta} autoCapitalize='none' />
+                <TextInput style={styles.input} cursorColor='#a0a0a0' selectionColor='#b5b5b5' placeholder='E-posta' onChangeText={onChangeEmail} value={email} autoCapitalize='none' />
                 <TextInput style={styles.input} cursorColor='#a0a0a0' selectionColor='#b5b5b5' placeholder='Şifre' secureTextEntry onChangeText={onChangePassword} value={password} autoCapitalize='none' />
             </View>
             <TouchableOpacity style={styles.button} onPress={onSubmit}>
