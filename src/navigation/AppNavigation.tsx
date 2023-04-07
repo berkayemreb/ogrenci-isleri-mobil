9f7f686f2bcdf34e3d58a225fc52363c1c99b6d2
bbogum19 <berkaye2001@hotmail.com>
2023-04-07 13:01:53 +0300

    redirection is done according to login process.

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../pages/Login';
 import HomeScreen from '../pages/Home';
 import { RootStackParamList } from './types';
+import { onAuthStateChanged } from "firebase/auth"
+import { auth } from '../firebase';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const AppNavigation = () => {
-    return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name='Login' component={LoginScreen} />
-            <Stack.Screen name='Home' component={HomeScreen}
-            />
-        </Stack.Navigator>
-    )
+
+  const [isLogin, setIsLogin] = useState<boolean>();
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      setIsLogin(!!user);
+    } else {
+
+    }
+  });
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {isLogin ? <Stack.Screen name='Home' component={HomeScreen} /> : <Stack.Screen name='Login' component={LoginScreen} />}
+    </Stack.Navigator>
+  )
 }
 
 export default AppNavigation;
