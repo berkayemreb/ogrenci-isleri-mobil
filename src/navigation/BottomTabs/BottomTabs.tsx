09d52fcd95c299927c392ffc185fea5ea658ae4c
bbogum19 <berkaye2001@hotmail.com>
2023-04-14 02:41:40 +0300

    icons and actions of tabs have been completed.

--- src/navigation/BottomTabs/BottomTabs.tsx
+++ src/navigation/BottomTabs/BottomTabs.tsx
@@ -3,16 +3,30 @@ import CommunicationStack from "../CommunicationStack";
 import EventStack from "../EventStack";
 import ExitStack from "../ExitStack";
 import MenuStack from "../MenuStack";
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabs = () => {
     return (
-        <Tab.Navigator initialRouteName="Menu">
-            <Tab.Screen name="Exit" component={ExitStack} options={{ headerShown: false }} />
-            <Tab.Screen name="Event" component={EventStack} options={{ headerShown: false }} />
-            <Tab.Screen name="Menu" component={MenuStack} options={{ headerShown: false }} />
-            <Tab.Screen name="Communication" component={CommunicationStack} options={{ headerShown: false }} />
+        <Tab.Navigator initialRouteName="Menu" screenOptions={{
+            tabBarStyle: { borderTopWidth: 0 },
+            tabBarLabel: () => { return null },
+            headerShown: false,
+        }}>
+            <Tab.Screen name="Communication" component={CommunicationStack} options={{
+                tabBarIcon: ({ focused }) => <MaterialCommunityIcons name="cellphone-marker" size={focused ? 26 : 22} color={focused ? '#FF7F50' : '#333333'} />
+            }} />
+            <Tab.Screen name="Event" component={EventStack} options={{
+                tabBarIcon: ({ focused }) => <MaterialIcons name="event-note" size={focused ? 26 : 22} color={focused ? '#FF7F50' : '#333333'} />
+            }} />
+            <Tab.Screen name="Menu" component={MenuStack} options={{
+                tabBarIcon: ({ focused }) => <MaterialIcons name="restaurant-menu" size={focused ? 26 : 22} color={focused ? '#FF7F50' : '#333333'} />
+            }} />
+            <Tab.Screen name="Exit" component={ExitStack} options={{
+                tabBarIcon: ({ focused }) => <MaterialCommunityIcons name="logout" size={focused ? 26 : 22} color={focused ? '#FF7F50' : '#333333'} />
+            }} />
         </Tab.Navigator>
     )
 }
