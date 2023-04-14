b21e8eb1b4b8a5e4519edb7e83ef377d2d9e8f20
bbogum19 <berkaye2001@hotmail.com>
2023-04-14 04:04:37 +0300

    design of the log out screen has been completed.

--- src/pages/ExitScreen/ExitScreen.tsx
+++ src/pages/ExitScreen/ExitScreen.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import styles from './ExitScreenStyles';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 
 const ExitScreen = () => {
+    const navigation = useNavigation<any>();
+
+    const goToMenuPage = () => {
+        navigation.navigate('Menu');
+    }
     return (
         <View style={styles.container}>
-            <Text> Exit Screen! </Text>
+            <View style={styles.inner_container}>
+                <View style={styles.message_container}>
+                    <Text style={styles.email_value}>test@gmail.com</Text>
+                    <Text style={styles.text}> hesabından çıkış yapmak istediğine emin misin?</Text>
+                </View>
+                <View style={styles.buttons_container}>
+                    <TouchableOpacity style={styles.exit_button}>
+                        <Text style={styles.text_of_exit_button}>Çıkış Yap</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.cancel_button} onPress={goToMenuPage}>
+                        <Text style={styles.text_of_cancel_button}>Vazgeç</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     )
 }
--- src/pages/ExitScreen/ExitScreenStyles.tsx
+++ src/pages/ExitScreen/ExitScreenStyles.tsx
@@ -1,8 +1,65 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
 
 export default StyleSheet.create({
     container: {
-        padding: 30,
-        backgroundColor: 'red'
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#c0c0c0'
+    },
+    inner_container: {
+        width: width / 1.75,
+        height: height / 3.25,
+        borderWidth: 1,
+        borderRadius: 6,
+        backgroundColor: '#fff',
+        borderColor: '#fff',
+        paddingHorizontal: 7,
+    },
+    message_container: {
+        flex: 0.75,
+        justifyContent: 'flex-end'
+    },
+    email_value: {
+        fontWeight: '600',
+        textDecorationLine: 'underline',
+        textAlign: 'center',
+        fontSize: 15,
+        color: '#333333',
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 15,
+        color: '#333333',
+    },
+    buttons_container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: -12,
+    },
+    exit_button: {
+        marginBottom: 6,
+        width: '85%',
+        backgroundColor: '#FF0111',
+        paddingVertical: 6,
+        borderRadius: 50,
+    },
+    text_of_exit_button: {
+        textAlign: 'center',
+        color: '#fff',
+    },
+    cancel_button: {
+        width: '85%',
+        backgroundColor: '#1E90FF',
+        paddingVertical: 6,
+        borderRadius: 50,
+    },
+    text_of_cancel_button: {
+        textAlign: 'center',
+        color: '#fff',
     }
 })
