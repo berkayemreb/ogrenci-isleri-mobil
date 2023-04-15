ac2b8f1f1e104310dfc1d8af35a9508c5d7fc871
bbogum19 <berkaye2001@hotmail.com>
2023-04-15 17:52:09 +0300

    map has been created.

--- src/pages/CommunicationScreen/CommunicationScreen.tsx
+++ src/pages/CommunicationScreen/CommunicationScreen.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, Button } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import styles from './CommunicationScreenStyles';
 
 const CommunicationScreen = () => {
+
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const region = {
+        latitude: 37.74525459245689,
+        longitude: 29.096191206200576,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.01,
+    };
+
+    const clickLocation = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <View style={styles.container}>
-            <Text> Communication Screen! </Text>
+            {isOpen &&
+                <View style={styles.popUpContainer}>
+                    <View style={styles.popUpInnerContainer}>
+                        <Text>Çalışma Saatleri:</Text>
+                        <Text>Telefon Numarası:</Text>
+                        <Button title='KAPAT' onPress={() => setIsOpen(!isOpen)} />
+                    </View>
+                </View>}
+            <MapView
+                style={styles.map}
+                region={region}
+            >
+                <Marker coordinate={region} onPress={clickLocation} />
+            </MapView>
         </View>
     )
 }
