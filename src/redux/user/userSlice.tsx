c102caaa15ae0f1f78f5bc9ee1a8060c6ed7445d
bbogum19 <berkaye2001@hotmail.com>
2023-04-28 17:54:41 +0300

    Redux has been edited for authentication

--- src/redux/user/userSlice.tsx
+++ src/redux/user/userSlice.tsx
@@ -1,20 +1,35 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 interface initialStateProps {
-
+    user: {
+        email: string | null;
+        uid: string | null;
+    }
 }
 
 const initialState: initialStateProps = {
+    user: {
+        email: null,
+        uid: null,
 
+    }
 }
 
 const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
+        setActiveUser: (state, action) => {
+            state.user.email = action.payload.email
+            state.user.uid = action.payload.uid
+        },
+        setUserLogout: state => {
+            state.user.email = null
+            state.user.uid = null
 
+        },
     }
 });
 
 export default userSlice.reducer;
-export const {} = userSlice.actions;
+export const { setActiveUser, setUserLogout } = userSlice.actions;
