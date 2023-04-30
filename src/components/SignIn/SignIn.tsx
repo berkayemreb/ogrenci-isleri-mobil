6ce2d1581d429aff058e2ba7222a2cff8d7fe37e
bbogum19 <berkaye2001@hotmail.com>
2023-04-30 20:18:25 +0300

    buttons and loading statuses on auth page edited.

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, TextInput, TouchableOpacity, Text } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { View, TextInput, TouchableOpacity, Text, Alert, ActivityIndicator } from 'react-native';
 import styles from './SignInStyles';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase';
@@ -11,6 +11,10 @@ const SignIn = () => {
 
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const emailRef = useRef<TextInput>(null);
+
 
     const dispatch = useDispatch();
 
@@ -21,8 +25,21 @@ const SignIn = () => {
     const onChangePassword = (text: string) => {
         setPassword(text);
     }
+    const showAlertMessage = () => {
+        Alert.alert(
+            'Giriş yapma işleminiz başarısız.',
+            'E-postanız veya şifreniz yanlıştır.',
+            [
+                {
+                    text: 'Tamam',
+                    onPress: () => emailRef.current?.focus(),
+                },
+            ]
+        );
+    }
 
     const onSubmit = () => {
+        setIsLoading(true);
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 // Signed in  
@@ -36,7 +53,8 @@ const SignIn = () => {
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
-                console.log("Giriş yapılamadı");
+                showAlertMessage();
+                setIsLoading(false);
                 setEmail('');
                 setPassword('');
             });
@@ -45,11 +63,11 @@ const SignIn = () => {
     return (
         <View style={styles.container}>
             <View>
-                <TextInput style={styles.input} cursorColor='#a0a0a0' selectionColor='#b5b5b5' placeholder='E-posta' onChangeText={onChangeEmail} value={email} autoCapitalize='none' />
+                <TextInput style={styles.input} cursorColor='#a0a0a0' selectionColor='#b5b5b5' placeholder='E-posta' onChangeText={onChangeEmail} value={email} autoCapitalize='none' ref={emailRef} />
                 <TextInput style={styles.input} cursorColor='#a0a0a0' selectionColor='#b5b5b5' placeholder='Şifre' secureTextEntry onChangeText={onChangePassword} value={password} autoCapitalize='none' />
             </View>
-            <TouchableOpacity style={styles.button} onPress={onSubmit} >
-                <Text style={styles.buttonText}>Giriş Yap</Text>
+            <TouchableOpacity style={styles.button} onPress={onSubmit} disabled={isLoading}>
+                <Text style={styles.buttonText}>{isLoading ? <ActivityIndicator color="#333333" /> : 'Giriş Yap'}</Text>
             </TouchableOpacity>
         </View>
     )
--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { View, TextInput, TouchableOpacity, Text } from 'react-native';
+import React, { useRef, useState } from "react";
+import { View, TextInput, TouchableOpacity, Text, Alert, ActivityIndicator } from 'react-native';
 import styles from './SignUpStyles';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase';
@@ -10,6 +10,9 @@ const SignUp = () => {
 
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const emailRef = useRef<TextInput>(null);
 
     const dispatch = useDispatch();
 
@@ -20,9 +23,32 @@ const SignUp = () => {
     const onChangePassword = (text: string) => {
         setPassword(text);
     }
-
+    const showAlertMessage = (errorCode?: string) => {
+        let message;
+        if (errorCode == 'auth/invalid-email') {
+            message = 'Geçersiz E-posta.';
+        } else if (errorCode == 'auth/weak-password') {
+            message = 'Geçersiz Şifre';
+        } else if (errorCode == 'auth/email-already-in-use') {
+            message = 'Bu E-posta ile zaten kayıt yapılmıştır.';
+        } else if (errorCode == 'auth/missing-password') {
+            message = 'Şifre alanı boş bırakılamaz.';
+        } else if (errorCode == 'auth/missing-email') {
+            message = 'E-posta alanı boş bırakılamaz.';
+        }
+        Alert.alert(
+            'Üye olma işleminiz başarısız.',
+            message,
+            [
+                {
+                    text: 'Tamam',
+                    onPress: () => emailRef.current?.focus(),
+                },
+            ]
+        );
+    }
     const onSubmit = () => {
-
+        setIsLoading(true);
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 // Signed in 
@@ -36,7 +62,8 @@ const SignUp = () => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
                 // ..
-                console.log("Üye olunamadı");
+                showAlertMessage(errorCode);
+                setIsLoading(false);
                 setEmail('');
                 setPassword('');
 
@@ -47,11 +74,11 @@ const SignUp = () => {
     return (
         <View style={styles.container}>
             <View>
-                <TextInput style={styles.input} cursorColor='#a0a0a0' selectionColor='#b5b5b5' placeholder='E-posta' onChangeText={onChangeEmail} value={email} autoCapitalize='none' />
+                <TextInput style={styles.input} cursorColor='#a0a0a0' selectionColor='#b5b5b5' placeholder='E-posta' onChangeText={onChangeEmail} value={email} autoCapitalize='none' ref={emailRef} />
                 <TextInput style={styles.input} cursorColor='#a0a0a0' selectionColor='#b5b5b5' placeholder='Şifre' secureTextEntry onChangeText={onChangePassword} value={password} autoCapitalize='none' />
             </View>
-            <TouchableOpacity style={styles.button} onPress={onSubmit}>
-                <Text style={styles.buttonText}>Üye Ol</Text>
+            <TouchableOpacity style={styles.button} onPress={onSubmit} disabled={isLoading}>
+                <Text style={styles.buttonText}>{isLoading ? <ActivityIndicator color="#333333" /> : 'Üye Ol'}</Text>
             </TouchableOpacity>
         </View>
 
