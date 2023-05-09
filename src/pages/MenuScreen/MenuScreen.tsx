af1a6f292403d05b396f8938eb587af901082a7b
bbogum19 <berkaye2001@hotmail.com>
2023-05-10 02:14:10 +0300

    created realtime db and get database

--- src/pages/MenuScreen/MenuScreen.tsx
+++ src/pages/MenuScreen/MenuScreen.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import styles from './MenuScreenStyles';
+import { getDatabase, ref, child, get } from "firebase/database";
+
 
 const MenuScreen = () => {
+
+    useEffect(() => {
+        const dbRef = ref(getDatabase());
+        get(child(dbRef, `menu`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                console.log(snapshot.val());
+            } else {
+                console.log("No data available");
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }, [])
+
     return (
         <View style={styles.container}>
             <Text> Menu Screen! </Text>
