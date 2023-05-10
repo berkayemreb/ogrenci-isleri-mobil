a8489bd7b584574074acfa6b1ef0ac57f3f63859
bbogum19 <berkaye2001@hotmail.com>
2023-05-11 00:07:12 +0300

    fetch data from realtime database has been edited.

--- src/pages/MenuScreen/MenuScreen.tsx
+++ src/pages/MenuScreen/MenuScreen.tsx
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import styles from './MenuScreenStyles';
 import { getDatabase, ref, child, get } from "firebase/database";
 
 
 const MenuScreen = () => {
+    const [menu, setMenu] = useState([]);
 
     useEffect(() => {
         const dbRef = ref(getDatabase());
         get(child(dbRef, `menu`)).then((snapshot) => {
             if (snapshot.exists()) {
-                console.log(snapshot.val());
+                console.log("firebaseden gelen hali", snapshot.val());
+                const data = snapshot.val();
+                const newMenu: any = Object.keys(data).map(key => ({
+                    id: key,
+                    ...data[key]
+                }));
+                console.log("düzenlendikten sonraki hali:", newMenu);
+                setMenu(newMenu);
+
             } else {
                 console.log("No data available");
             }
@@ -22,6 +31,14 @@ const MenuScreen = () => {
     return (
         <View style={styles.container}>
             <Text> Menu Screen! </Text>
+            {menu.map((item: any, index: any) => {
+                return (
+                    <View key={index}>
+                        <Text>{item.name}</Text>
+                        <Text>{item.price}</Text>
+                    </View>
+                )
+            })}
         </View>
     )
 }
