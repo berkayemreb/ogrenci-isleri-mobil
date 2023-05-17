06b4e40a842ae71dd8fc3bafbbaf1db81237360c
bbogum19 <berkaye2001@hotmail.com>
2023-05-18 02:51:52 +0300

    background color of exit page has been changed.

--- src/pages/ExitScreen/ExitScreenStyles.tsx
+++ src/pages/ExitScreen/ExitScreenStyles.tsx
@@ -8,7 +8,7 @@ export default StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#B9C4CF',
+        backgroundColor: '#d5d5d5',
 
     },
     inner_container: {
