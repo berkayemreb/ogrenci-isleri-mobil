9a6bd80095350ff2eaec2e1b27a6c6052c174ad7
bbogum19 <berkaye2001@hotmail.com>
2023-05-30 01:41:56 +0300

    events page has been updated.

--- src/pages/ScreensOfEvent/EventScreen/EventScreen.tsx
+++ src/pages/ScreensOfEvent/EventScreen/EventScreen.tsx
@@ -1,14 +1,17 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { SafeAreaView, View, Text, TouchableOpacity, Pressable, TextInput } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, View, Text, TouchableOpacity, FlatList } from 'react-native';
 import styles from './EventScreenStyles';
 import { useNavigation } from '@react-navigation/native';
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../../firebase';
 import { useIsFocused } from "@react-navigation/native";
 
-const EventScreen = () => {
+interface EventProps {
+    id: any,
+    editorName: any,
+}
 
-    const isFocused = useIsFocused();
+const EventScreen = () => {
 
     const navigation: any = useNavigation();
 
@@ -16,30 +19,41 @@ const EventScreen = () => {
         navigation.navigate('CreateEventScreen');
     }
 
-    const [documentData, setDocumentData] = useState<any>([]);
+    const [events, setEvents] = useState<EventProps[]>([]);
+    const isFocused = useIsFocused();
+
 
     useEffect(() => {
-        console.log("useEffect içi");
+        const fetchEvents = async () => {
+            try {
+                const eventsRef = collection(db, 'events');
+                const snapshot = await getDocs(eventsRef);
+
+                const fetchedEvents: any = snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    ...doc.data()
+                }));
+
+                setEvents(fetchedEvents);
+            } catch (error) {
+                console.log('Firestore verileri çekilirken bir hata oluştu:', error);
+            }
+        };
+
         if (isFocused) {
-            const fetchData = async () => {
-                const querySnapshot = await getDocs(collection(db, "events"));
-                querySnapshot.forEach((doc) => {
-                    console.log("docData:", doc.data());
-                    setDocumentData([...documentData, doc.data()])
-                });
-            };
-            fetchData();
+            fetchEvents();
         }
+    }, [isFocused]);
 
-    }, [isFocused])
+    // ... bileşenin geri kalan kısmı ...
+    console.log('DATA:', events);
 
-    console.log("ARRAY:", documentData);
-
+    const renderEvent = ({ item }: any) => (<Event item={item} />)
 
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.normal_flow_container}>
-                <Text>Etkinlikeri gör</Text>
+                <FlatList data={events} renderItem={renderEvent} keyExtractor={(item) => item.id.toString()} />
             </View>
             <View style={styles.out_flow_container}>
                 <TouchableOpacity onPress={goToCreateEventScreen}>
@@ -48,9 +62,17 @@ const EventScreen = () => {
                     </View>
                 </TouchableOpacity>
             </View>
-        </SafeAreaView>
-
+        </SafeAreaView >
     )
 }
 
 export default EventScreen;
+
+const Event = (props: any) => {
+    return (
+        <TouchableOpacity onPress={() => { console.log('tıklanan Id:', props.item.id) }} >
+            <Text>{props.item.editorName}</Text>
+            <Text>{props.item.id}</Text>
+        </TouchableOpacity>
+    )
+}
